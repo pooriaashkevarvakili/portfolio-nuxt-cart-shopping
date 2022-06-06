a03577c975535fad28457a6f449c0c47d91382ad
pooriavakili <pooriavakili09@gmail.com>
2022-06-06 10:57:28 +0430

    addprojectnuxt

---
+++ store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+export const state = () => ({
+    todo: 'mohamad'
+})
+export const getters = () => ({
+
+})
+export const mutations = () => ({
+
+})
+export const actions = () => ({
+
+})
